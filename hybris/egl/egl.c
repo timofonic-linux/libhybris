d2e71ae39b3020fa5a41a3758de68181e70963d3
NeKit <nekit1000@gmail.com>
2017-10-18 23:12:51 +0300

    Allow window system to hook eglGetConfigAttrib (needed for X11 EGL_NATIVE_VISUAL_ID)

--- hybris/egl/egl.c
+++ hybris/egl/egl.c
@@ -228,7 +228,6 @@ const char * eglQueryString(EGLDisplay dpy, EGLint name)
 
 HYBRIS_IMPLEMENT_FUNCTION4(egl, EGLBoolean, eglGetConfigs, EGLDisplay, EGLConfig *, EGLint, EGLint *);
 HYBRIS_IMPLEMENT_FUNCTION5(egl, EGLBoolean, eglChooseConfig, EGLDisplay, const EGLint *, EGLConfig *, EGLint, EGLint *);
-HYBRIS_IMPLEMENT_FUNCTION4(egl, EGLBoolean, eglGetConfigAttrib, EGLDisplay, EGLConfig, EGLint, EGLint *);
 
 EGLSurface eglCreateWindowSurface(EGLDisplay dpy, EGLConfig config,
 		EGLNativeWindowType win,
@@ -459,6 +458,18 @@ EGLBoolean eglDestroyImageKHR(EGLDisplay dpy, EGLImageKHR image)
 		return EGL_TRUE;
 	}
 	return ret;
+}
+
+EGLBoolean eglGetConfigAttrib(EGLDisplay dpy, EGLConfig config, EGLint attribute, EGLint *value)
+{
+	HYBRIS_DLSYSM(egl, &_eglGetConfigAttrib, "eglGetConfigAttrib");
+	struct _EGLDisplay *display = hybris_egl_display_get_mapping(dpy);
+
+    EGLBoolean ret = ws_eglGetConfigAttrib(display, config, attribute, value);
+    if (ret == EGL_FALSE) {
+        return (*_eglGetConfigAttrib)(dpy, config, attribute, value);
+    }
+    return ret;
 }
 
 // vim:ts=4:sw=4:noexpandtab
